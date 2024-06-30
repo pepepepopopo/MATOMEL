225870f3115fb420ab5162dba01589881fd2854f
pepepepopopo <timeflies325@gmail.com>
2024-06-30 23:13:15 +0900

    ヘッダーにテキストを配置

--- front_matomel/src/Matomel.js
+++ front_matomel/src/Matomel.js
@@ -6,9 +6,37 @@ const Matomel= ()  => {
   return (
     <>
       <div className="header">
-        <a hef="">
-          <img src="" className="App-logo" alt="logo" />
-        </a>
+        <div className="MATOMELlogo">
+          <a hef="">
+            <img src="" className="App-logo" />
+          </a>
+          <a hef="">MATOMEL</a>
+        </div>
+        <div className="newArrival">
+          <a hef="">
+            <img src="" className="newArrival-logo" />
+          </a>
+          <a hef="">新着</a>
+        </div>
+        <div className="trend">
+          <a hef="">
+            <img src="" className="trend-logo" />
+          </a>
+          <a hef="">トレンド</a>
+        </div>
+        <div className="tag">
+          <a hef="">
+            <img src="" className="tag-logo"/>
+          </a>
+          <a hef="">タグ</a>
+        </div>
+        <div className="mypage">
+
+        </div>
+      </div>
+      <div className="serchBar">
+        <input></input>
+        
       </div>
     </>
   );
